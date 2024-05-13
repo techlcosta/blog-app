c170e22b178ec96e92262f254245da1bf9622204
Leandro Costa <techlcosta@MacBook-Pro-de-Leandro.local>
2024-05-13 22:24:58 +0900

    Resolve conflitos entre branches

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://api.techlcosta.dev/api',
-=======
   baseURL: import.meta.env.VITE_API_URL,
->>>>>>> c4b1371 (add docker configs)
+
   withCredentials: true
 })
